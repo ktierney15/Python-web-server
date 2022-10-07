9370bcab7b0c312cd5cc6788734a77ed010ec869
Kevin Tierney <kevintierney@Kevins-Air-2.fios-router.home>
2022-10-07 11:51:32 -0400

    fixed client

--- client.py
+++ client.py
@@ -1,6 +1,20 @@
-import requests
-
-url = 'http://localhost:8080/'
-obj = {"Test": "Test"}
-
-requests.post(url, data = obj)
+import http.client  
+ 
+#get http server ip  
+http_server = "localhost"
+port = "8080"  
+#create a connection  
+conn = http.client.HTTPConnection(http_server, port)  
+ 
+#request command to server  
+conn.request("GET", http://localhost:8080)  
+ 
+#get response from server  
+rsp = conn.getresponse()  
+ 
+#print server response and data  
+print(rsp.status, rsp.reason)  
+data_received = rsp.read()  
+print(data_received)  
+ 
+conn.close()  
